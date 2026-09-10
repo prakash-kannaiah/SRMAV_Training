0cf4895aa8902b5fcace7eedd08851fab0f842cb
Prakash Kannaiah <105844116+prakash-kannaiah@users.noreply.github.com>
2026-09-10 15:21:36 +0530

    Create Publisher.py

---
+++ Publisher.py
@@ -0,0 +1,35 @@
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import String
+
+
+class Talker(Node):
+
+    def __init__(self):
+        super().__init__('talker')
+
+        # Create publisher
+        self.publisher = self.create_publisher(String, 'chatter', 10)
+
+        # Create timer
+        self.timer = self.create_timer(1.0, self.timer_callback)
+
+    def timer_callback(self):
+        msg = String()
+        msg.data = 'Hello ROS 2'
+        self.publisher.publish(msg)
+        self.get_logger().info(f'Publishing: "{msg.data}"')
+
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    node = Talker()
+    rclpy.spin(node)
+
+    node.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
